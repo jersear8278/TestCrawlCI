24a2cd8635622c5b4190bc10dae17ba8658295f3
jersear8278 <jersear8278@gmail.com>
2017-11-28 09:39:40 +0800

    test

--- crawl.go
+++ crawl.go
@@ -11,7 +11,6 @@ import (
 func main() {
     resp, err := http.Get("http://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20171125&stockNo=2330&_=1511567214171")
     if err != nil {
-        // handle error
     }
 
     defer resp.Body.Close()
